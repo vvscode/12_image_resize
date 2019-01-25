928d7806a16d25b287d861e3b32d57e5c2651ce5
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-25 18:50:32 +0400

    Implemet resize logic

--- image_resize.py
+++ image_resize.py
@@ -5,7 +5,7 @@ from PIL import Image
 import re
 
 
-def pil_image_input(path):
+def pil_image_input_type(path):
     if not os.path.isfile(path):
         raise argparse.ArgumentTypeError("File not exists")
 
@@ -14,16 +14,14 @@ def pil_image_input(path):
     except OSError:
         raise argparse.ArgumentTypeError("File is not an image")
 
-
-def resize_image(path_to_original, path_to_result):
-    pass
-
+def get_image_size(pil_image):
+    return pil_image.width, pil_image.height
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="devman image resizer script"
     )
-    parser.add_argument("input_file", help="Path to image for resizing", type=pil_image_input)
+    parser.add_argument("input_file", help="Path to image for resizing", type=pil_image_input_type)
     parser.add_argument('--output', help='Path for output file', type=argparse.FileType('r'))
     parser.add_argument('--scale', metavar='scale', type=float,
                         help='How many times to enlarge the image (maybe less than 1)')
@@ -37,47 +35,34 @@ if __name__ == '__main__':
         sys.exit("You can't use `scale` param and `width`/`height` together")
 
     input_image_path, input_image = args.input_file
-    scale = args.scale
-    width = args.width
-    height = args.height
-    output_path = args.output
+    input_scale = args.scale
+    input_width = args.width
+    input_height = args.height
+    input_result_path = args.output
 
     current_width = input_image.width
     current_height = input_image.height
     current_proportion = current_height / current_width
 
-    if scale:
-        target_width = int(current_width * scale)
-        target_height = int(current_height * scale)
-    elif width and height:
-        target_height = height
-        target_width = width
+    if input_scale:
+        target_width = int(current_width * input_scale)
+        target_height = int(current_height * input_scale)
+    elif input_width and input_height:
+        target_height = input_height
+        target_width = input_width
         if target_height / target_width != current_proportion:
             print("The proportions do not match")
-    elif width:
-        target_width = width
+    elif input_width:
+        target_width = input_width
         target_height = int(current_proportion * target_width)
     else:
-        target_height = height
+        target_height = input_height
         target_width = int(target_height / current_proportion)
 
-    target_output_path = output_path or re.sub(r'(\.\w+?)?$', r'__{}x{}\1'.format(target_width, target_height),
-                                        input_image_path, count=1)
+    target_result_path = input_result_path or re.sub(r'(\.\w+?)?$', r'__{}x{}\1'.format(target_width, target_height),
+                                                     input_image_path, count=1)
 
-    data = {
-        "input_image_path": input_image_path,
-        "scale": scale,
-        "width": width,
-        "height": height,
-        "output_path": output_path,
+    output_image = input_image.resize((target_width, target_height))
+    output_image.save(target_result_path)
 
-        "current_width": current_width,
-        "current_height": current_height,
-        "current_proportion": current_proportion,
-
-        "target_width": target_width,
-        "target_height": target_height,
-        "target_output_path":target_output_path
-    }
-    for key in data:
-        print(key, data[key])
+    print("File was saved to `{}`".format(target_result_path))
